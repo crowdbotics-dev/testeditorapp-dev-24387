9cf1703b8f7f8572d9462519b90a3a2e70beb66f
Crowdbotics <engineering@crowdbotics.com>
2022-05-13 05:46:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Testdata(models.Model):
+    "Generated Model"
+    name = models.TextField()
--- backend/testeditorapp_dev_24387/wsgi.py
+++ backend/testeditorapp_dev_24387/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testeditorapp_dev_24387.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testeditorapp_dev_24387.settings")
 
 application = get_wsgi_application()
